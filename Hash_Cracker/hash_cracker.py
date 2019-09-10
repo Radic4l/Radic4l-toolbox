a473bc72d3d2ab6897de93a73e8384048af475db
Radic4l <axel1991@hotmail.fr>
2019-09-10 23:07:33 +0200

    merge

--- Hash_Cracker/hash_cracker.py
+++ Hash_Cracker/hash_cracker.py
@@ -1,7 +1,7 @@
-#!usr/bin/env python
-## Hash_Cracker v0.1 written by Radic4l
+# #!usr/bin/env python
+# ## Hash_Cracker v0.1 written by Radic4l
 import platform
-import hashlib
+import hashlib, sys
 from optparse import OptionParser
 import subprocess
 
@@ -10,7 +10,7 @@ parser = OptionParser('''
 
         -w :        wordlist
         --sha256 :  option for sha256
-        --md5 :     option for md5 
+        --md5 :     option for md5
 
         Exemple : python hash_cracker.py -w rockyou.txt --sha256 <hash>
         ''')
@@ -23,33 +23,74 @@ while True:
     try:
         (options, args) = parser.parse_args()
         wordlist = options.wordlist
-        wordlist = open(wordlist,'r',encoding='latin-1')
         break
     except all:
-        #print('No file found \n')
+        print('No file found \n')
         parser.error("try again ...")
-        parser.print_help
 
-hash_count = 0
-wordlist = wordlist.readlines()
+wordlst = open(wordlist, 'r', encoding='latin-1')
+callBash = f"wc -l {wordlist} | cut -d ' ' -f1"
 
-for word in wordlist:
-    word = word.strip('\n')
-    if options.sha256:
-        wordlist_hash = hashlib.sha256(word.encode('latin-1')).hexdigest()
-        hash = options.sha256
-    elif options.md5:
-        wordlist_hash = hashlib.md5(word.encode('latin-1')).hexdigest()
-        hash = options.md5
+toto = subprocess.Popen(callBash,stdout=subprocess.PIPE,shell=True)
+wordlistLenght = toto.stdout.read().decode('utf-8').replace("\n","")
 
-    if hash == wordlist_hash:
-        hash_count = hash_count + 1
-        print('Hash FOUND : '+word+' || Hash : '+wordlist_hash)
-        break
-    else:
-        hash_count = hash_count + 1
-        print('Current Hash : '+wordlist_hash+'\nHash tested : ({0}/{1})'.format(hash_count,len(wordlist)))
-        # if platform.system()=='Linux':
-        #     subprocess.call('clear', shell=True)
-        # else:
-        #     subprocess.call('cls',shell=True)
+with wordlst as fp:
+    line = fp.readline()
+    cnt = 1
+    while line:
+        # print("Line {}: {}".format(cnt, line.strip()))
+        # input('Read next line ...')
+        cnt += 1
+        if options.sha256:
+            line_hash = hashlib.sha256(line.encode('latin-1')).hexdigest()
+            hash = options.sha256
+        elif options.md5:
+            line_hash = hashlib.md5(line.encode('latin-1')).hexdigest()
+            hash = options.md5
+
+        toto = f'Current Hash : {line_hash} \nHash tested : ({cnt}/{wordlistLenght})'
+        digits = len(str(len(toto) - 1))
+        delete = "\b" * (digits)
+        # input()
+        if hash == line_hash:
+            cnt += 1
+            print(f'Hash FOUND : {line} || Hash : {line_hash}')
+            break
+        else:
+            # print("{0}{1:{2}}".format(delete, toto, digits))
+            print(toto, sep=' ', end='', flush=True)
+            # print(toto,end='\n',flush=True)
+        # sys.stdout.flush()
+        line = fp.readline()  # mettre a la fin de if elif else etc ...
+        # print(wordlistLenght)
+        # input('Continue ...')
+fp.close()
+
+# filepath = 'rockyou.txt'
+
+# hash_count = 0
+# # wordlist = wordlist.readlines()
+# wordlist = wordlist.readline()
+#
+#
+#
+# # for word in wordlist:
+# #     word = word.strip('\n')
+# #     if options.sha256:
+# #         wordlist_hash = hashlib.sha256(word.encode('latin-1')).hexdigest()
+# #         hash = options.sha256
+# #     elif options.md5:
+# #         wordlist_hash = hashlib.md5(word.encode('latin-1')).hexdigest()
+# #         hash = options.md5
+# #
+# #     if hash == wordlist_hash:
+# #         hash_count = hash_count + 1
+# #         print('Hash FOUND : '+word+' || Hash : '+wordlist_hash)
+# #         break
+# #     else:
+# #         hash_count = hash_count + 1
+# #         print('Current Hash : '+wordlist_hash+'\nHash tested : ({0}/{1})'.format(hash_count,len(wordlist)))
+# #         # if platform.system()=='Linux':
+# #         #     subprocess.call('clear', shell=True)
+# #         # else:
+# #         #     subprocess.call('cls',shell=True)
