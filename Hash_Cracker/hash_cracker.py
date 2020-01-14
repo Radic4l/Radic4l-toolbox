9e3b61417297b2b0996478a2c1870984f93b4230
Radic4l <axel.dujardin@campus-numerique-in-the-alps.com>
2020-01-14 09:33:02 +0100

    ajout Sha-1

--- Hash_Cracker/hash_cracker.py
+++ Hash_Cracker/hash_cracker.py
@@ -1,7 +1,7 @@
 # #!usr/bin/env python
 # ## Hash_Cracker v0.1 written by Radic4l
-import platform
-import hashlib, sys
+import hashlib
+import mmap
 from optparse import OptionParser
 import subprocess
 
@@ -18,6 +18,7 @@ parser = OptionParser('''
 parser.add_option("-w",dest="wordlist",help="Path/to/wordlist.txt")
 parser.add_option("--md5",dest="md5",help="Enter your md5 hash")
 parser.add_option("--sha256",dest="sha256",help="Enter your sha256 hash")
+parser.add_option("--sha1",dest="sha1",help="Enter your sha1 hash")
 
 while True:
     try:
@@ -31,66 +32,41 @@ while True:
 wordlst = open(wordlist, 'r', encoding='latin-1')
 callBash = f"wc -l {wordlist} | cut -d ' ' -f1"
 
-toto = subprocess.Popen(callBash,stdout=subprocess.PIPE,shell=True)
-wordlistLenght = toto.stdout.read().decode('utf-8').replace("\n","")
+file_lenght = subprocess.Popen(callBash,stdout=subprocess.PIPE,shell=True)
+wordlistLenght = file_lenght.stdout.read().decode('latin-1').replace("\n","")
 
 with wordlst as fp:
+    cnt = 0
     line = fp.readline()
-    cnt = 1
+    # print([i for i in line],end='\n')
+
     while line:
-        # print("Line {}: {}".format(cnt, line.strip()))
-        # input('Read next line ...')
         cnt += 1
         if options.sha256:
-            line_hash = hashlib.sha256(line.encode('latin-1')).hexdigest()
-            hash = options.sha256
+            line_hash = hashlib.sha256(line.strip().encode()).hexdigest()
+            passed_hash = options.sha256
         elif options.md5:
-            line_hash = hashlib.md5(line.encode('latin-1')).hexdigest()
-            hash = options.md5
+            line_hash = hashlib.md5(line.strip().encode()).hexdigest()
+            passed_hash = options.md5
+            # print([i for i in line],end='\n')
+        elif options.sha1:
+            line_hash = hashlib.sha1(line.strip().encode()).hexdigest()
+            passed_hash = options.sha1
+            # print([i for i in line],end='\n')
+        else:
+            print('Choose hash type ...')
 
-        toto = f'Current Hash : {line_hash} \nHash tested : ({cnt}/{wordlistLenght})'
-        digits = len(str(len(toto) - 1))
-        delete = "\b" * (digits)
+        toto = f'Current Hash : {line_hash}/{passed_hash} ({cnt}/{wordlistLenght})'
         # input()
-        if hash == line_hash:
+        if line_hash == passed_hash:
             cnt += 1
-            print(f'Hash FOUND : {line} || Hash : {line_hash}')
+            print('',end='\n\r')
+            # print(f'''
+            # \n{'='*int(len(toto)/2-5)}HASH  FOUND{'='*int(len(toto)/2-5)}\n{toto}\nResult Hash  : {line}
+            # ''')
             break
         else:
-            # print("{0}{1:{2}}".format(delete, toto, digits))
-            print(toto, sep=' ', end='', flush=True)
-            # print(toto,end='\n',flush=True)
-        # sys.stdout.flush()
-        line = fp.readline()  # mettre a la fin de if elif else etc ...
-        # print(wordlistLenght)
-        # input('Continue ...')
-fp.close()
-
-# filepath = 'rockyou.txt'
-
-# hash_count = 0
-# # wordlist = wordlist.readlines()
-# wordlist = wordlist.readline()
-#
-#
-#
-# # for word in wordlist:
-# #     word = word.strip('\n')
-# #     if options.sha256:
-# #         wordlist_hash = hashlib.sha256(word.encode('latin-1')).hexdigest()
-# #         hash = options.sha256
-# #     elif options.md5:
-# #         wordlist_hash = hashlib.md5(word.encode('latin-1')).hexdigest()
-# #         hash = options.md5
-# #
-# #     if hash == wordlist_hash:
-# #         hash_count = hash_count + 1
-# #         print('Hash FOUND : '+word+' || Hash : '+wordlist_hash)
-# #         break
-# #     else:
-# #         hash_count = hash_count + 1
-# #         print('Current Hash : '+wordlist_hash+'\nHash tested : ({0}/{1})'.format(hash_count,len(wordlist)))
-# #         # if platform.system()=='Linux':
-# #         #     subprocess.call('clear', shell=True)
-# #         # else:
-# #         #     subprocess.call('cls',shell=True)
+            line = fp.readline()
+            # print(f'''{'='*int(len(toto)/2-5)}{'='*int(len(toto)/2-5)}''',toto,sep='',end='\r',file=sys.stdout,flush=True)
+            # print(f'''\r{'='*int(len(toto)/2-5)}Progress..  {'='*int(len(toto)/2-5)}''')
+            print(f'{toto}',end='\r')
