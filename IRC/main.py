c2b1c813c828e666670f93526c522e314d8f0f74
Radic4l <axel.dujardin@campus-numerique-in-the-alps.com>
2020-03-12 08:49:29 +0100

    connection granted

--- IRC/main.py
+++ IRC/main.py
@@ -1,21 +1,33 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import irc3,socket, struct
+import time,socket
 
 HOST = 'irc.root-me.org'
 PORT = 6667
 ADDRESS = (HOST,PORT)
-CHANNEL = '#root-me_challenge'
 BOTNAME = 'candy'
 
-
+def send_data(command):
+    soc.send(command)
 soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 input(f'[+] Connecting to irc {HOST}')
 soc.connect((HOST,PORT))
-input('OK ! Sending simply packet ...')
-packet = struct.pack('!B', 0)
-soc.sendto(packet,ADDRESS)
-print('Message Sent ! Try to receive ...')
-data,_ = soc.recvfrom(1024)
-input('Received message !')
-print(f'DATA : {data}')
+input('Enter for sent ID')
+send_data(b'USER titi tutu tyty :tete \r\n')
+time.sleep(5)
+send_data(b'NICK titi \r\n')
+time.sleep(5)
+send_data(b'JOIN #root-me_challenge \r\n')
+time.sleep(5)
+send_data(b':titi JOIN #root-me_challenge \r\n')
+data= soc.recv(1024).decode()
+print(data)
+
+while data:
+    data = soc.recv(1024).decode()
+    print(data)
+
+# try:
+#     soc.connect((HOST,PORT))
+#     print(f'[Connected to {HOST}')
+#     try:
